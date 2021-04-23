6d57bc194f911fc8d03d5bd9576166ddf36e370c
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-04-23 16:53:43 +0200

    first commit

---
+++ Assignment 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string : ");
+            string charMessenge = Console.ReadLine();
+            
+
+            string text = Words.CountCharactersInSentence(charMessenge);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Assignment 4/Words.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_4
+{
+    class Words
+    {
+        public static string CountCharactersInSentence(string inputString)
+        {
+            string intNumbers = inputString;
+            string strLetters = ""; // this will be used at the end of the code
+
+            intNumbers = intNumbers.Replace(" ", string.Empty).ToLower();
+            while (intNumbers != "")// loops checks if thre's an empty string if not
+            {
+                char letters = intNumbers[0];//chaning the string to an array and the [0] is
+                int echo = 0;//created an interger to count the individual characters
+                for (int count = 0; count < intNumbers.Length; count++)//length is loops through the entire string
+                {
+                    if (intNumbers[0] == intNumbers[count])// to compare the array string wordNumber[0] to string 
+                    {
+                        echo++;
+                    }
+                }
+                strLetters += $" {letters} -> {echo} ";
+                intNumbers = intNumbers.Replace(new String(letters, 1), string.Empty);
+                //initialising the first character of the of the string
+            }
+            return strLetters;
+        }
+    }
+}
